df55a83ad2c9b847ca981fa5e970dd17f297a67c
GonzaloBeltranCatala <43141599+GonzaloBeltranCatala@users.noreply.github.com>
2018-12-17 15:45:13 +0100

    Add files via upload

--- Proyecto_Convivencia_Formulario 2/cerebro/formulario/investigadoresListar.js
+++ Proyecto_Convivencia_Formulario 2/cerebro/formulario/investigadoresListar.js
@@ -1,12 +1,20 @@
 $.getJSON( "incidencias", function( data ) {
   var items = [];
     $.each( data, function( key, val ) {
-	console.log(key);
-    items.push( "<li id='" + key + "' class='list-group-item'>" + val.alumne + "</li>" );
+  console.log(key);
+  
+  //items.push( "<li id='" + key + "' class='list-group-item'>" + val.alumne + val.profesor + "</li>");
+
+  var fila="<tr><td>"+val.alumne+"</td><td>"+val.grupo+"</td><td>"+val.profesor+"</td><td>"+val.data+"</td></tr>"
+
+
+  $( "#tablalista" ).append(fila);
+
   });
  
-  $( "<ul/>", {
-    "class": "list-group",
-    html: items.join( "" )
-  }).appendTo( "#divLista" );
+  //$( "<ul/>", {
+    //"class": "list-group",
+    //html: items.join( "" )
+  //}).appendTo( "#divLista" );
 });
+ 
